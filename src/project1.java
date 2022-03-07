ea9d459529cb2be9af38b3b12bcd83056b8c8b3a
Sun Jung <100884647+sunnny10@users.noreply.github.com>
2022-03-07 14:00:54 +0900

    Update project1.java

--- src/project1.java
+++ src/project1.java
@@ -1,7 +1,6 @@
 
 public class project1 {
 	public static void main(String[] args) {
-		System.out.println("project1 test");
-		System.out.println("fetch test");
+		System.out.println("branch1");
 	}
 }

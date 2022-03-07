b51c96f7ab3ababe5efe8461bbd796915f3cfada
Sun Jung <wjdtjs905@likelion.org>
2022-03-07 14:25:46 +0900

    Revert "test"
    This reverts commit cc423fcc7629c61b5a8a5d81340a74acfdcedff3.

--- src/project1.java
+++ src/project1.java
@@ -2,7 +2,6 @@
 public class project1 {
 	public static void main(String[] args) {
 		System.out.println("project1 test");
-		System.out.println("되돌리기 실험 중....");
-		
+		System.out.println("fetch test");
 	}
 }

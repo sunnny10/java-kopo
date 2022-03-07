cc423fcc7629c61b5a8a5d81340a74acfdcedff3
Sun Jung <wjdtjs905@likelion.org>
2022-03-07 14:23:33 +0900

    test

--- src/project1.java
+++ src/project1.java
@@ -2,6 +2,7 @@
 public class project1 {
 	public static void main(String[] args) {
 		System.out.println("project1 test");
-		System.out.println("fetch test");
+		System.out.println("되돌리기 실험 중....");
+		
 	}
 }
